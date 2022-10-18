b3f81b61133da11e8fb03a4af31be694b83634f1
utkarsh3mehta <umteappdev@gmail.com>
2022-10-18 20:52:26 +0530

    transactions queue prod and consumer added

--- receiver.ts
+++ receiver.ts
@@ -1,20 +1,95 @@
-import * as amqp from "amqplib/callback_api";
+import * as Bull from "bull";
+import { transactions } from "./api";
+import { AxiosError } from "axios";
+import * as dotenv from 'dotenv'
 
-amqp.connect("amqp://127.0.0.1", (err0, connection) => {
-  if (err0) {console.log(err0); throw err0};
-  connection.createChannel((err1, channel) => {
-    if (err1) throw err1;
-    var queue = "journey_v1";
-    channel.assertQueue(queue, {
-      durable: false,
-    });
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-    channel.consume(
-      queue,
-      (message) => {
-        console.log(" [x] Received message: ", message.content.toString());
-      },
-      { noAck: false }
-    );
-  });
-});
+dotenv.config();
+/**
+ * steps:
+ * create a queue
+ * every 5 minutes, add 'make API calls' to queue
+ * 
+ *
+ */
+const onAllProcess = (job: Bull.Job, done: Bull.DoneCallback) => {
+  // let now = new Date();
+  // let then = new Date(now);
+  // then.setMinutes(then.getMinutes() - 5);
+  console.log('added all transactions API')
+  done();
+  // transactions
+  //   .get("/api/marketing/external/get-all-transactions")
+  //   .then((res) => {})
+  //   .catch((err) => {});
+};
+const onFnbProcess = (job: Bull.Job, done: Bull.DoneCallback) => {
+  // let now = new Date();
+  // let then = new Date(now);
+  // then.setMinutes(then.getMinutes() - 5);
+  console.log('added fnb transactions API');
+  done();
+  // transactions
+  //   .get("/api/marketing/external/get-all-fnb-transactions")
+  //   .then((res) => {})
+  //   .catch((err) => {});
+};
+function onActive(job: Bull.Job, jobPromise: Bull.JobPromise) {
+  console.log("activating", job.queue.name, job.id);
+}
+function onWaiting(jobId: Bull.JobId) {
+  console.log("waiting for", jobId);
+}
+function onCleaned(jobs: Bull.Job[], status: Bull.JobStatusClean) {
+  console.log(
+    "cleaned job ids",
+    jobs.map((j) => (j.id, j.queue)),
+    status
+  );
+}
+function onCompleted(job: Bull.Job, result: any) {
+  console.log("completed", job.queue.name, job.id, "result", result);
+}
+function onDrained() {
+  console.log("drained");
+}
+function onError(err: Error) {
+  console.log("error", err);
+}
+function onFailed(job: Bull.Job, err: Error) {
+  console.log("failed", job.queue.name, job.id, "error", err);
+}
+function onPaused() {
+  console.log("paused");
+}
+function onProgress() {
+  console.log("progress updated");
+}
+function onRemoved(job: Bull.Job) {
+  console.log("removed", job.queue.name, job.id);
+}
+function onResumed() {
+  console.log("resumed");
+}
+function onStalled(job: Bull.Job) {
+  console.log("stalled", job.queue.name, job.id);
+}
+
+const receiverApiCalls = () => {
+  const transactions = new Bull(process.env.TRANSACTIONS);
+  transactions.on("active", onActive);
+  transactions.on("cleaned", onCleaned);
+  transactions.on("completed", onCompleted);
+  transactions.on("drained", onDrained);
+  transactions.on("error", onError);
+  transactions.on("failed", onFailed);
+  transactions.on("paused", onPaused);
+  transactions.on("progress", onProgress);
+  transactions.on("removed", onRemoved);
+  transactions.on("resumed", onResumed);
+  transactions.on("stalled", onStalled);
+  transactions.on("waiting", onWaiting);
+  transactions.process("all", onAllProcess);
+  transactions.process("fnb", onFnbProcess);
+};
+
+receiverApiCalls();
