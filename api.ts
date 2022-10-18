7ffd087a4de4df79bef3d04f8a66153d573455c2
utkarsh3mehta <umteappdev@gmail.com>
2022-10-18 20:51:37 +0530

    added transactions API

--- api.ts
+++ api.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
+import * as dotenv from 'dotenv'
+
+dotenv.config();
 
 const api = axios.create({
-  baseURL: 'http://localhost:8001/api/v1/'
-})
+  baseURL: "http://localhost:8001/api/v1/",
+});
 
 export default api;
+
+export const transactions = axios.create({
+  baseURL: "https://brij-api-v1.empirecinemas.com",
+  headers: {
+    Authorization:
+      `Bearer ${process.env.AUTH_TOKEN}`,
+  },
+});
