7226ac39b86654370d5bb04a7ba0619cceef6102
utkarsh3mehta <umteappdev@gmail.com>
2022-10-24 14:16:31 +0530

    added API call to add user to segment

--- workers/actions/add-to-segment.ts
+++ workers/actions/add-to-segment.ts
@@ -1,5 +1,16 @@
-import { Data } from "../../types";
+import { Data, MessageData } from "../../types";
+import api from "../../api";
 
-export const addToSegment = async (data: Data) => {
-  console.log("added to segment");
+export const addToSegment = async (data: MessageData) => {
+  try {
+    let segmentId = parseInt(data.node.data.selectedValue["add-to-segment"]);
+    let payload = {
+      segmentId: segmentId,
+      contactId: data.data.user_id,
+    };
+    const response = await api.post(`segment/customer/seg/${segmentId}`, payload);
+    if (!response) {
+      console.error('error adding user to segment');
+    }
+  } catch (e) {}
 };
