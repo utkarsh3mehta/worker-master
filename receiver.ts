9dc7de279f2193b307f75f384ad3d0df96e6d0c4
utkarsh3mehta <umteappdev@gmail.com>
2022-10-31 16:10:42 +0530

    TODO: transactions 5 minutes

--- receiver.ts
+++ receiver.ts
@@ -2,7 +2,7 @@ import * as Bull from "bull";
 import api, { transactions } from "./api";
 import { AxiosError } from "axios";
 import * as dotenv from "dotenv";
-import { Data, iJourneyMaster } from "./types";
+import { AllData, FnBData, iJourneyMaster, MessageData } from "./types";
 import { startJourney } from "./main";
 
 dotenv.config();
@@ -16,73 +16,136 @@ dotenv.config();
  * start journey for each
  */
 
-let transactionIds: String[] = [];
-let transactionsFnbIds: String[] = [];
+let transactionIds: number[] = [];
+let transactionsFnbIds: number[] = [];
 let bookedJourneys: iJourneyMaster[] = [];
 let fnbJourneys: iJourneyMaster[] = [];
 
 const onAllProcess = (job: Bull.Job, done: Bull.DoneCallback) => {
-  // let now = new Date();
-  // let then = new Date(now);
-  // then.setMinutes(then.getMinutes() - 5);
-  // console.log("added all transactions API");
-  transactions
-    .get("/api/marketing/external/get-all-transactions")
+  console.log("processing all", job.queue.name, job.id);
+  api
+    .post("journey/trigger-type/get", { triggerType: "booked-ticket" })
     .then((res) => {
-      let transactions: Data[] = res.data;
-      let ids = transactions.map((t) => t._id.$oid);
-      let newids: String[] = [];
-      for (let id of ids) {
-        if (!transactionIds.includes(id)) {
-          newids.push(id);
-        }
-      }
-      if (transactionIds.length >= 20) {
-        for (let i = 0; i < newids.length; i++) {
-          transactionIds.shift();
-        }
-      }
-      transactionIds.push(...newids);
-      newids.forEach((id) => {
-        let transaction = transactions.find((t) => t._id.$oid === id);
-        bookedJourneys.forEach((j) => {
-          startJourney(j, transaction);
+      bookedJourneys = res.data.data;
+      // console.log("booked journeys", bookedJourneys);
+      let now = new Date();
+      let then = new Date(now);
+      then.setDate(then.getDate() - 1);
+      transactions
+        .get(
+          `/api/marketing/external/get-all-transactions?fromDate=${then.getFullYear()}-${
+            then.getMonth() + 1
+          }-${then.getDate()}&toDate=${now.getFullYear()}-${
+            now.getMonth() + 1
+          }-${now.getDate()}`
+        )
+        .then(async (res) => {
+          // console.log("all transactions response", res.data);
+          let transactions: AllData[] = res.data;
+          let ids = transactions.map((t) => t.transaction_number);
+          console.log("transactions", ids);
+          let newids: number[] = [];
+          for (let id of ids) {
+            if (!transactionIds.includes(id)) {
+              newids.push(id);
+            }
+          }
+          console.log("new ids", newids);
+          if (transactionIds.length >= 20) {
+            for (let i = 0; i < newids.length; i++) {
+              transactionIds.shift();
+            }
+          }
+          console.log("existing transactions ids", transactionIds);
+          transactionIds.push(...newids);
+          for (const id of newids) {
+            let transaction = transactions.find(
+              (t) => t.transaction_number === id
+            );
+            for (const j of bookedJourneys) {
+              console.log("starting journey", j.name, "for id", id);
+              await startJourney(j, transaction);
+            }
+          }
+          done();
+        })
+        .catch((err: AxiosError) => {
+          console.log("err", JSON.stringify(err));
+          done(err);
         });
-      });
-      done();
     })
-    .catch((err: AxiosError) => {});
+    .catch((err) => {
+      done(new Error("Error getting journeys"), err);
+    });
 };
 const onFnbProcess = (job: Bull.Job, done: Bull.DoneCallback) => {
-  // let now = new Date();
-  // let then = new Date(now);
-  // then.setMinutes(then.getMinutes() - 5);
-  // console.log("added fnb transactions API");
-  done();
-  // transactions
-  //   .get("/api/marketing/external/get-all-fnb-transactions")
-  //   .then((res) => {})
-  //   .catch((err) => {});
+  console.log("processing fnb", job.queue.name, job.id);
+  api
+    .post("journey/trigger-type/get", { triggerType: "ordered-f-and-b" })
+    .then((res) => {
+      fnbJourneys = res.data.data;
+      // console.log("fnb journeys", fnbJourneys);
+      let now = new Date();
+      let then = new Date(now);
+      then.setDate(then.getDate() - 1);
+      transactions
+        .get(
+          `/api/marketing/external/get-all-fnb-transactions?fromDate=${then.getFullYear()}-${
+            then.getMonth() + 1
+          }-${then.getDate()}&toDate=${now.getFullYear()}-${
+            now.getMonth() + 1
+          }-${now.getDate()}`
+        )
+        .then(async (res) => {
+          console.log("fnb transactions response", res.data);
+          let transactions: FnBData[] = res.data.Records.data;
+          let ids = transactions.map((t) => t.transaction_number);
+          console.log("fnb ids", ids);
+          let newids: number[] = [];
+          for (let id of ids) {
+            if (!transactionsFnbIds.includes(id)) {
+              newids.push(id);
+            }
+          }
+          console.log("new fnb ids", newids);
+          if (transactionsFnbIds.length >= 20) {
+            for (let i = 0; i < newids.length; i++) {
+              transactionsFnbIds.shift();
+            }
+          }
+          console.log("existing fnb transaction ids", transactionsFnbIds);
+          transactionsFnbIds.push(...newids);
+          for (const id of newids) {
+            // console.log("newid", id);
+            let transaction = transactions.find(
+              (t) => t.transaction_number === id
+            );
+            // for (const j of fnbJourneys) {
+            //   console.log("journey", j.name);
+            //   console.log(
+            //     "starting journey",
+            //     j.name,
+            //     "for data number",
+            //     transaction.transaction_number
+            //   );
+            // }
+            for (const j of fnbJourneys) await startJourney(j, transaction);
+          }
+          done(null);
+        })
+        .catch((err) => {
+          done(new Error("Error getting transactions"), err);
+        });
+    })
+    .catch((err) => {
+      done(new Error("Error getting journeys"), err);
+    });
 };
 function onActive(job: Bull.Job, jobPromise: Bull.JobPromise) {
   console.log("activating", job.queue.name, job.id);
 }
 function onWaiting(jobId: Bull.JobId) {
   console.log("waiting for", jobId);
-  api
-    .post("journey/trigger-type/get", { triggerType: "booked-ticket" })
-    .then((res) => {
-      let data: iJourneyMaster[] = res.data;
-      bookedJourneys = data;
-    })
-    .catch((err) => {});
-  api
-    .post("journey/trigger-type/get", { triggerType: "ordered-fnb" })
-    .then((res) => {
-      let data: iJourneyMaster[] = res.data;
-      fnbJourneys = data;
-    })
-    .catch((err) => {});
 }
 function onCleaned(jobs: Bull.Job[], status: Bull.JobStatusClean) {
   console.log(
