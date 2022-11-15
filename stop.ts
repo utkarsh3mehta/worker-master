bd8b440275b3956d8db9a89fa4de48dc4d5372f4
utkarsh3mehta <umteappdev@gmail.com>
2022-11-15 11:31:36 +0530

    added a stop script

---
+++ stop.ts
@@ -0,0 +1,18 @@
+import { createClient } from "redis";
+import { RepeatJobs } from "./types";
+import * as Bull from "bull";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const stopRepeatJobs = async () => {
+  const transactions = new Bull(process.env.TRANSACTIONS);
+  const repeatJobs = await transactions.getRepeatableJobs();
+  console.log("repeat jobs", repeatJobs);
+  for (const job of repeatJobs) {
+    await transactions.removeRepeatableByKey(job.key);
+  }
+  process.exit(0);
+};
+
+stopRepeatJobs();
